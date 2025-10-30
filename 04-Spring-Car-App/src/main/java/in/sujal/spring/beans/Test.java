869cb98a9dafaff3d7e2c68fe222f8739d51468c
SUJAL40 <sujalkhot2479@gmail.com>
2025-10-30 22:16:49 +0530

    AutoWiring byName
    AutoWiring based on ::
    byName , byType :- for setter injection
    constructor
    none

--- 04-Spring-Car-App/src/main/java/in/sujal/spring/beans/Test.java
+++ 04-Spring-Car-App/src/main/java/in/sujal/spring/beans/Test.java
@@ -10,10 +10,8 @@ public class Test {
 		//load the class and create the object
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		Car c1 = context.getBean(Car.class);
-//		c1.drive();
-		System.out.println(c1.hashCode());
-		Car c2 = context.getBean(Car.class);
-		System.out.println(c2.hashCode());
+		c1.drive();
+		
 	}
 
 }
