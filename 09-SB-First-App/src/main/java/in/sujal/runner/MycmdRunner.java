c1e942e5312a7d9c9037c2355a94d44b4dc39c3d
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-06 18:12:29 +0530

    Add application startup runners for initialization tasks
    Introduces MyAppRunner (ApplicationRunner) and MycmdRunner (CommandLineRunner) to execute setup logic immediately after the Spring Boot context is initialized.

---
+++ 09-SB-First-App/src/main/java/in/sujal/runner/MycmdRunner.java
@@ -0,0 +1,14 @@
+package in.sujal.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MycmdRunner implements CommandLineRunner{
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("MycmdRunner...rum() method.....");
+		
+	}
+
+}
