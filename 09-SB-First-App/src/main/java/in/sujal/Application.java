23417c6919961b3b0dab23e642bff682e8cc3a85
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-06 17:23:47 +0530

    Initial setup for basic Spring Boot REST application
    Creates the core Application class using @SpringBootApplication and includes a simple @RestController with a root ("/") endpoint. This establishes the foundation for the new project.

---
+++ 09-SB-First-App/src/main/java/in/sujal/Application.java
@@ -0,0 +1,23 @@
+package in.sujal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctxt = SpringApplication.run(Application.class, args);
+		System.out.println(ctxt.getClass().getName());
+	}
+	
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to New Journey";
+	}
+
+}
---
+++ 09-SB-First-App/src/main/java/in/sujal/dao/ReportDao.java
@@ -0,0 +1,8 @@
+package in.sujal.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReportDao {
+
+}
