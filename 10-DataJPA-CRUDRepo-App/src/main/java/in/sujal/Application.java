2aecea3eb740ed1f49b1bda6ed07cd06ab87ed81
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-07 12:11:07 +0530

    CrudRepository methods
    here we use different crud repository method for curd operation on database

--- 10-DataJPA-CRUDRepo-App/src/main/java/in/sujal/Application.java
+++ 10-DataJPA-CRUDRepo-App/src/main/java/in/sujal/Application.java
@@ -1,5 +1,7 @@
 package in.sujal;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
@@ -12,11 +14,13 @@ import in.sujal.repo.BookRepository;
 @SpringBootApplication
 public class Application {
 
+	private static final String BookRepository = null;
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 		BookRepository repo = context.getBean(BookRepository.class);
 		//IOC container will return implementing class object
-		System.out.println(repo.getClass().getName());
+//		System.out.println(repo.getClass().getName());
 		
 //		Book b = new Book();
 //		b.setBookId(101);
@@ -27,9 +31,85 @@ public class Application {
 		
 		//To retrieve data form database
 		
-		Optional<Book> id = repo.findById(101);
-		//repo.findById() give Optional object to avoid null pointer exception
-		System.out.println(id.toString());
+//		Optional<Book> id = repo.findById(101);
+//		//repo.findById() give Optional object to avoid null pointer exception
+//		System.out.println(id.toString());
+		
+		//saveAll() it insert multiple record
+		
+//		Book b2 = new Book();
+//		b2.setBookId(102);
+//		b2.setBookName("Angular");
+//		b2.setBookPrice(2500.00);
+//		
+//		Book b3 = new Book();
+//		b3.setBookId(103);
+//		b3.setBookName("Java");
+//		b3.setBookPrice(500.00);
+//		
+//		repo.saveAll(Arrays.asList(b2,b3));
+//		
+//		System.out.println("Data Saved Successfully.....");
+		
+		//check id is present or not
+		
+//		boolean status = repo.existsById(102);
+//		System.out.println("Record present ::"+status);
+//		
+//		//Get record count of table
+//		
+//		System.out.println("Record Count::"+repo.count());
+//		
+//		Iterable<Book> all = repo.findAll(); //->select * from table_name
+//		for(Book b:all) {
+//			System.out.println(b);
+//		}
+//		
+//		//findAllById()
+//		
+//		Iterable<Book> ids = repo.findAllById(Arrays.asList(101,103));
+//		for(Book b:ids) {
+//			System.out.println(b);
+//		}
+//		
+		
+		//delete the record
+		
+//		repo.deleteById(105);
+		
+		//Requirement :: Retrieve books whose price >=1000
+		
+//		List<Book> books = repo.findByBookPriceGreaterThanEqual(1000.00);
+//		for(Book b:books) {
+//			System.out.println(b);
+//		}
+		
+		//get all data using custom query -> native query
+		
+		List<Book> allBooks = repo.getAllBooks();
+		for(Book b:allBooks) {
+			System.out.println(b);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
--- 10-DataJPA-CRUDRepo-App/src/main/java/in/sujal/repo/BookRepository.java
+++ 10-DataJPA-CRUDRepo-App/src/main/java/in/sujal/repo/BookRepository.java
@@ -1,9 +1,17 @@
 package in.sujal.repo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import in.sujal.entity.Book;
 
 public interface BookRepository extends CrudRepository<Book, Integer> {
-
+	
+	public List<Book> findByBookPriceGreaterThanEqual(Double bookprice);
+	
+	@Query(value="select * from book",nativeQuery=true)
+	public List<Book> getAllBooks();
+	
 }
