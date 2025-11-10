5c62c2f7ff9ca1753f3e78287a4c883238ebd7bb
SUJAL40 <sujalkhot2479@gmail.com>
2025-11-10 12:10:28 +0530

    First Spring Web MVC Project
    In this we add spring web and devetools dependency in pom.xml , add massageController with @GetMappint() and added jps as a presentation technology

---
+++ 13-Web-MVC-App1/src/main/java/in/sujal/controller/MsgController.java
@@ -0,0 +1,26 @@
+package in.sujal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MsgController {
+	
+	@GetMapping("/welcome")
+	public ModelAndView getWelcomeMsg() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("msg","Welcome To Ashok It.....");
+		mav.setViewName("message");
+		return mav;
+	}
+	
+	@GetMapping("/Greet")
+	public ModelAndView getGrettingMsg() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("msg","Good Morning.....");
+		mav.setViewName("message");
+		return mav;
+	}
+
+}
